4aea9fccd819b18ca804f2a8269298ece957ef94
Elias-sara <eliasgisara@gmail.com>
2024-10-06 23:56:22 +0300

    Updated server.js with logging and error handling

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ import express from "express";
 import mongoose from "mongoose";
 import cors from "cors";
 import dotenv from "dotenv";
+import morgan from "morgan"; // For logging HTTP requests
+
 dotenv.config();
 
 // Import routes
@@ -25,6 +27,7 @@ mongoose
 app.use(cors());
 app.use(express.json()); // For parsing application/json
 app.use(express.urlencoded({ extended: true })); // For parsing application/x-www-form-urlencoded
+app.use(morgan("dev")); // HTTP request logging
 
 // Use routes
 app.use("/api/orders", orderRoutes);
@@ -34,6 +37,12 @@ app.use("/uploads", express.static("uploads")); // Serve static files from the u
 // Define a route for the root URL
 app.get("/", (req, res) => {
   res.send("Welcome to the My Perfume Backend API!");
+});
+
+// Error handling middleware
+app.use((err, req, res, next) => {
+  console.error(err.stack); // Log error stack for debugging
+  res.status(500).send("Something broke!"); // Send a generic error message
 });
 
 // Start the server
